1b594f4fd36bd6ea47b5038b99c8aacefb5c7b82
Toriola Faidat <faaedah@gmail.com>
2022-01-15 12:58:39 +0100

    api fetches successfully

--- main.js
+++ main.js
@@ -1,8 +1,19 @@
-const apiKey = config.SECRET_API_KEY;
-console.log(apiKey)
+const apiKey = config.SECRET_API_KEY;  //config is the object name, not file name(config.js) 
+const input = document.querySelector('input')
 
-fetch('https://api.openweathermap.org/data/2.5/weather?q=Lagos&appid=a49eee2baed2b1b29d1897bbb49b2f66')
-.then( (res) => {
-    console.log(res.json())
-})
-.catch((err) => {console.log(err)})
+function showCity() {
+    let cityName = input.value
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`)
+    .then( (res) => {
+        console.log(res.json())
+    })
+    .catch((err) => {console.log(err)})
+}
+
+
+document.addEventListener("keyup", function(e) {
+    if (e.code === 'Enter') {
+        showCity()
+        cityName = ""
+    }
+});

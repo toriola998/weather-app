f5a60dc31f39627e7d9e954a659d7ef309418906
Toriola Faidat <faaedah@gmail.com>
2022-01-15 12:38:35 +0100

    added api

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+const apiKey = config.SECRET_API_KEY;
+console.log(apiKey)
 
 fetch('https://api.openweathermap.org/data/2.5/weather?q=Lagos&appid=a49eee2baed2b1b29d1897bbb49b2f66')
 .then( (res) => {

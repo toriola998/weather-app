c9e8d9bf70104d2313f8da19773d8defa6c2f048
Toriola Faidat <faaedah@gmail.com>
2022-01-15 10:35:52 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,6 @@
+
+fetch('https://api.openweathermap.org/data/2.5/weather?q=Lagos&appid=a49eee2baed2b1b29d1897bbb49b2f66')
+.then( (res) => {
+    console.log(res.json())
+})
+.catch((err) => {console.log(err)})
